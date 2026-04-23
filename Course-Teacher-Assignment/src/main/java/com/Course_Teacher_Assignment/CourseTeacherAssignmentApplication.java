5457bce8d426c160fce4be37532ee0af078c54aa
harikumar-star <harikumaru.99@gmail.com>
2026-04-23 13:52:50 +0530

    first commit

---
+++ Course-Teacher-Assignment/src/main/java/com/Course_Teacher_Assignment/CourseTeacherAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.Course_Teacher_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseTeacherAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseTeacherAssignmentApplication.class, args);
+	}
+
+}
---
+++ EnrollMentService/src/main/java/com/EnrollMentService/EnrollMentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.EnrollMentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollMentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollMentServiceApplication.class, args);
+	}
+
+}
